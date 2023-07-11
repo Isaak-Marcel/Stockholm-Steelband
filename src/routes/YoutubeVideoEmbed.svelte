6cee9517d89aab6100e30c696b854d6e35d4558d
Isaak-Marcel <isflakisaak@gmail.com>
2023-07-11 22:11:36 +0200

    added comments for updateing the server

--- src/routes/YoutubeVideoEmbed.svelte
+++ src/routes/YoutubeVideoEmbed.svelte
@@ -3,7 +3,7 @@
     <div class="flex justify-center"><iframe class="my-10 " width="660" height="375" src="https://www.youtube.com/embed/I71_uJ1Wm6s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
 
     
-
+    <!-- //fwa -->
     <!-- <video muted autoplay playsinline disablepictureinpicture loop class="w-full object-cover" controls>
         <source src="videos\Stockholm steelband showreel film 3.mp4" />
     </video> -->
